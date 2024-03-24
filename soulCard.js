05f1f634b9dc78554e37f8012494754f886eb3dd
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-03-23 21:10:09 -0700

    Update soulCard.js

--- soulCard.js
+++ soulCard.js
@@ -1,26 +1,26 @@
 // Function to generate a random soul card
-function generateSoulCard(traits, virtues, sins, maleFirstNames, femaleFirstNames, lastNames) {
-    const gender = Math.random() < 0.5 ? 'male' : 'female';
-    const firstName = gender === 'male' ? getRandomItem(maleFirstNames) : getRandomItem(femaleFirstNames);
-    const lastName = getRandomItem(lastNames);
-    const age = getRandomNumber(18, 100);
-    const trait1 = getRandomItem(traits);
-    const trait2 = getRandomItem(traits);
-    const virtue = getRandomItem(virtues);
-    const sin = getRandomItem(sins);
-
-    const totalKarma = virtue.score + sin.score;
-
-    return {
-        firstName,
-        lastName,
-        gender,
-        age,
-        traits: [trait1, trait2],
-        virtue,
-        sin,
-        totalKarma
-    };
+function generateSoulCardASCII(soulCard) {
+    const ascii = `
+    +--------------------------+
+    |  ${soulCard.firstName} ${soulCard.lastName}  |
+    +--------------------------+
+    |  Gender: ${soulCard.gender}              |
+    |  Age: ${soulCard.age}                  |
+    |                          |
+    |  Traits:                 |
+    |  - ${soulCard.traits[0]}               |
+    |  - ${soulCard.traits[1]}               |
+    |                          |
+    |  Virtue: ${soulCard.virtue.name}      |
+    |  (${soulCard.virtue.score})             |
+    |                          |
+    |  Sin: ${soulCard.sin.name}          |
+    |  (${soulCard.sin.score})                 |
+    |                          |
+    |  Total Karma: ${soulCard.totalKarma}        |
+    +--------------------------+
+    `;
+    return ascii;
 }
 
 // Function to get a random item from an array
