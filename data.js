15dc2a1cfc85e70568030a4c40942ac72a479014
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-03-23 20:10:48 -0700

    Update data.js

--- data.js
+++ data.js
@@ -6,16 +6,19 @@ async function loadJSON(url) {
 
 // Load game data
 async function loadGameData() {
-    const [traits, virtuesData, sinsData, realms] = await Promise.all([
+    const [traits, virtuesData, sinsData, realms, maleFirstNames, femaleFirstNames, lastNames] = await Promise.all([
         loadJSON('data/traits.json'),
         loadJSON('data/virtues.json'),
         loadJSON('data/sins.json'),
-        loadJSON('data/realms.json')
+        loadJSON('data/realms.json'),
+        loadJSON('data/maleFirstNames.json'),
+        loadJSON('data/femaleFirstNames.json'),
+        loadJSON('data/lastNames.json')
     ]);
 
     // Convert virtues and sins data to arrays of objects
     const virtues = Object.entries(virtuesData).map(([name, score]) => ({ name, score }));
     const sins = Object.entries(sinsData).map(([name, score]) => ({ name, score }));
 
-    return { traits, virtues, sins, realms };
+    return { traits, virtues, sins, realms, maleFirstNames, femaleFirstNames, lastNames };
 }
